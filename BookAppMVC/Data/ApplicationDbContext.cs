be2c38c977552b0e8cfed1a5ecb40b3917740b1b
Andrew TOwnsley <atownsley86@gmail.com>
2022-06-17 09:07:56 -0600

    Working in getting DB created in MSSMS

--- BookAppMVC/Data/ApplicationDbContext.cs
+++ BookAppMVC/Data/ApplicationDbContext.cs
@@ -1,13 +1,18 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using BookAppMVC.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace BookAppMVC.Data
 {
+
     public class ApplicationDbContext : IdentityDbContext
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
+
         }
+
+        public DbSet<Category> categories { get; set; }
     }
 }
