028331147f9ea25a6f7e3fdfbbdfc9b060f1364c
Andrew TOwnsley <atownsley86@gmail.com>
2022-06-16 22:22:57 -0600

    MVC app booklist

---
+++ BookAppMVC/Models/Category.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BookAppMVC.Models
+{
+    public class Category
+    {
+        [Key]
+
+        public int myProperty { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        public int DisplayOrder { get; set; }
+
+        public DateTime CreatedDateTime { get; set; } = DateTime.Now;
+    }
+}
